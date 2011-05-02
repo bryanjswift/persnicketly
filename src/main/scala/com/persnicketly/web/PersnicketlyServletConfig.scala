94abf882afc20d3b2b21b6a9aebd58542183677f
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-02 01:21:09 -0400

    Add Guice servlet config for use as a context-listener

---
+++ src/main/scala/com/persnicketly/web/PersnicketlyServletConfig.scala
@@ -0,0 +1,24 @@
+package com.persnicketly.web
+
+import org.slf4j.LoggerFactory
+import com.google.inject.servlet.{GuiceServletContextListener, ServletModule}
+import com.google.inject.{Guice, Injector, Singleton}
+import com.sun.jersey.spi.container.servlet.ServletContainer
+import com.sun.jersey.api.core.PackagesResourceConfig
+import scala.collection.JavaConversions.asJavaMap
+
+class PersnicketlyServletConfig extends GuiceServletContextListener {
+  private val log = LoggerFactory.getLogger(classOf[PersnicketlyServletConfig])
+  override protected def getInjector():Injector = Guice.createInjector(new PersnicketlyServletModule)
+}
+
+private class PersnicketlyServletModule extends ServletModule {
+  private val log = LoggerFactory.getLogger(classOf[PersnicketlyServletModule])
+  override protected def configureServlets():Unit = {
+    val jerseyParams = Map(PackagesResourceConfig.PROPERTY_PACKAGES -> "com.persnicketly.web.resource,com.codahale.jersey.providers,com.codahale.jersey.providers,com.codahale.jersey.inject")
+    serve("/*").`with`(classOf[JerseyServletContainer], jerseyParams)
+  }
+}
+
+@Singleton
+private class JerseyServletContainer extends ServletContainer { }
