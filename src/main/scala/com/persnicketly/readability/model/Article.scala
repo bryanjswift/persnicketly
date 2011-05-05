352e683062bc32fbef66bececeefcf39a59f08a4
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-05 04:47:41 -0400

    Removed some unnecessary fields and re-organized

--- src/main/scala/com/persnicketly/readability/model/Article.scala
+++ src/main/scala/com/persnicketly/readability/model/Article.scala
@@ -1,15 +1,8 @@
 package com.persnicketly.readability.model
 
-import org.joda.time.DateTime
-
 case class Article(
-  domain: String,
+  articleId: String,
   title: String,
   url: String,
-  excerpt: Option[String],
-  word_count: Option[Int],
-  id: String,
-  author: Option[String],
-  date_publish: Option[String],
-  next_page_href: Option[String])
+  excerpt: Option[String])
 
