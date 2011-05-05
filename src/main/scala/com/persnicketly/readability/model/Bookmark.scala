6701eebcf97adf735bece43c781a3bbb104c1a63
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-05 13:49:00 -0400

    Move date formatting from Bookmark constructor to data extractor

--- src/main/scala/com/persnicketly/readability/model/Bookmark.scala
+++ src/main/scala/com/persnicketly/readability/model/Bookmark.scala
@@ -1,7 +1,6 @@
 package com.persnicketly.readability.model
 
 import org.joda.time.DateTime
-import org.joda.time.format.DateTimeFormat
 
 case class Bookmark(
   bookmarkId: Int,
@@ -12,21 +11,4 @@ case class Bookmark(
   archivedDate: Option[DateTime],
   favoritedDate: Option[DateTime],
   updatedDate: Option[DateTime])
-
-object Bookmark {
-  val format = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss")
-  def apply(archivedDate: Option[String], favoritedDate: Option[String], updatedDate: Option[String],
-            bookmarkId: Int, userId: Int, isFavorite: Boolean, isArchived: Boolean, article: Article):Bookmark = {
-
-    Bookmark(
-      bookmarkId,
-      userId,
-      isFavorite,
-      isArchived,
-      article,
-      archivedDate.map(format.parseDateTime),
-      favoritedDate.map(format.parseDateTime),
-      updatedDate.map(format.parseDateTime))
-  }
-}
 
--- src/main/scala/com/persnicketly/readability/model/BookmarkExtractor.scala
+++ src/main/scala/com/persnicketly/readability/model/BookmarkExtractor.scala
@@ -2,17 +2,16 @@ package com.persnicketly.readability.model
 
 import dispatch.json.Js._
 import dispatch.json.{Extract, JsValue, JsObject}
+import org.joda.time.format.DateTimeFormat
 
 object BookmarkExtractor extends Extract[Bookmark] {
+  private val format = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss")
   def unapply(js: JsValue) = js match {
     case JsObject(m) => 
       if (BookmarkJson.keys.forall(k => m.contains(k))) {
         // can safely extract data
         Some(
           Bookmark(
-            BookmarkJson.favoritedDate.unapply(js),
-            BookmarkJson.archivedDate.unapply(js),
-            BookmarkJson.updatedDate.unapply(js),
             BookmarkJson.bookmarkId(js).toInt,
             BookmarkJson.userId(js).toInt,
             BookmarkJson.isFavorite(js),
@@ -22,7 +21,10 @@ object BookmarkExtractor extends Extract[Bookmark] {
               BookmarkJson.ArticleJson.url(js),
               BookmarkJson.ArticleJson.articleId(js),
               BookmarkJson.ArticleJson.excerpt.unapply(js)
-            )
+            ),
+            BookmarkJson.favoritedDate.unapply(js).map(format.parseDateTime),
+            BookmarkJson.archivedDate.unapply(js).map(format.parseDateTime),
+            BookmarkJson.updatedDate.unapply(js).map(format.parseDateTime)
           )
         )
       } else {
