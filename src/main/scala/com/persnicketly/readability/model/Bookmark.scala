8ab57a035cd13f8f315c5092502c66e967a7e200
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-05 04:58:54 -0400

    Make it possible to pass Option[String] or Option[DateTime] for dates

--- src/main/scala/com/persnicketly/readability/model/Bookmark.scala
+++ src/main/scala/com/persnicketly/readability/model/Bookmark.scala
@@ -1,5 +1,6 @@
 package com.persnicketly.readability.model
 
+import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 
 case class Bookmark(
@@ -8,11 +9,24 @@ case class Bookmark(
   isFavorite: Boolean,
   isArchived: Boolean,
   article: Article,
-  archivedDate: Option[String],
-  favoritedDate: Option[String],
-  updatedDate: Option[String])
+  archivedDate: Option[DateTime],
+  favoritedDate: Option[DateTime],
+  updatedDate: Option[DateTime])
 
 object Bookmark {
   val format = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss")
+  def apply(archivedDate: Option[String], favoritedDate: Option[String], updatedDate: Option[String],
+            bookmarkId: Int, userId: Int, isFavorite: Boolean, isArchived: Boolean, article: Article):Bookmark = {
+
+    Bookmark(
+      bookmarkId,
+      userId,
+      isFavorite,
+      isArchived,
+      article,
+      archivedDate.map(format.parseDateTime),
+      favoritedDate.map(format.parseDateTime),
+      updatedDate.map(format.parseDateTime))
+  }
 }
 
