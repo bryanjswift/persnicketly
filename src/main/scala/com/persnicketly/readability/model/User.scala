35830c2b1718feec9bf8ff245109c0fc376354f3
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-08 12:25:16 -0400

    Add UserData information and easier was to extract tokens

--- src/main/scala/com/persnicketly/readability/model/User.scala
+++ src/main/scala/com/persnicketly/readability/model/User.scala
@@ -3,5 +3,31 @@ package com.persnicketly.readability.model
 import org.bson.types.ObjectId
 import dispatch.oauth.Token
 
-case class User(_id: Option[ObjectId], requestToken: Token, accessToken: Option[Token], verifier: Option[String])
+case class User(
+  id: Option[ObjectId],
+  requestToken: Token,
+  accessToken: Option[Token],
+  verifier: Option[String],
+  personalInfo: Option[UserData])
 
+object TokenHelper {
+  def apply(value: Option[String], secret: Option[String]) = {
+    if (value.isDefined && secret.isDefined) {
+      Some(Token(value.get, secret.get))
+    } else {
+      None
+    }
+  }
+}
+
+case class UserData(username: String, firstName: String, lastName: String)
+
+object UserData {
+  def apply(username: Option[String], firstName:Option[String], lastName: Option[String]): Option[UserData] = {
+    if (username.isDefined && firstName.isDefined && lastName.isDefined) {
+      Some(UserData(username.get, firstName.get, lastName.get))
+    } else {
+      None
+    }
+  }
+}
