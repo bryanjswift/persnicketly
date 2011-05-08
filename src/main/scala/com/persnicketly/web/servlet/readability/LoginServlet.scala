d0ea183beb45cafd34a797364a18b5e2dd572840
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-08 10:41:17 -0400

    Make the login servlet persist user data rather than store in memory

--- src/main/scala/com/persnicketly/web/servlet/readability/LoginServlet.scala
+++ src/main/scala/com/persnicketly/web/servlet/readability/LoginServlet.scala
@@ -2,7 +2,9 @@ package com.persnicketly.web.servlet.readability
 
 import dispatch.nio.Http
 import com.google.inject.Singleton
+import com.persnicketly.persistence.UserDao
 import com.persnicketly.readability.Auth
+import com.persnicketly.readability.model.User
 import com.persnicketly.web.{Persnicketly, Servlet}
 import org.slf4j.LoggerFactory
 import dispatch.oauth.Token
@@ -15,6 +17,9 @@ class LoginServlet extends Servlet {
     val handler = Auth.request_token(Persnicketly.oauthConsumer, Persnicketly.oauthCallback)
     val http = new Http
     val token = http.apply(handler)()
+    // save user
+    val userId = UserDao.save(User(None, token, None, None))
+    // set cookie to ObjectId of User
     LoginServlet.tokens += (token.value -> token)
     log.info("auth_token = {}", token)
     val authorizeUrl = Auth.authorize_url(token).to_uri.toString
