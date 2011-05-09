6bea318683dc47b88b68d7eb5b3c3d056632976d
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-09 19:51:15 -0400

    Set mpq at the start of tracking rather than the end

--- src/main/webapp/js/tracking.js
+++ src/main/webapp/js/tracking.js
@@ -1,4 +1,9 @@
-var mpq = (function(doc, tag, win) {
+(function(doc, tag, win) {
+  win.mpq = [
+    ["init","b13487272824c77655227e84f830d7ee"],
+    ["register", params],
+    ["track", "pageview"]
+  ];
   var s = doc.createElement(tag)
     , el = doc.getElementsByTagName(tag)[0]
     , nav = win.navigator
@@ -11,9 +16,4 @@ var mpq = (function(doc, tag, win) {
   s.src = "//api.mixpanel.com/site_media/js/api/mixpanel.js";
   s.async = 1;
   el.parentNode.insertBefore(s, el);
-  return [
-    ["init","b13487272824c77655227e84f830d7ee"],
-    ["register", params],
-    ["track", "pageview"]
-  ];
 })(document, "script", window);
