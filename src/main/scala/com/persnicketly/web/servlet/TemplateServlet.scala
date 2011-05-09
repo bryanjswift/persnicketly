0c76377f51ba9537b0324a6e4ba13bc97cc8d6df
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-08 22:15:46 -0400

    Add a generic template servlet

---
+++ src/main/scala/com/persnicketly/web/servlet/TemplateServlet.scala
@@ -0,0 +1,19 @@
+package com.persnicketly.web.servlet
+
+import com.google.inject.Singleton
+import com.persnicketly.web.Servlet
+import velocity.VelocityView
+import org.slf4j.LoggerFactory
+import javax.ws.rs.core.MediaType
+
+@Singleton
+class TemplateServlet extends Servlet {
+  private val log = LoggerFactory.getLogger(classOf[TemplateServlet])
+  override def doGet(helper: HttpHelper) {
+    log.info("Rendering view - /template{}.vm", helper.uri)
+    val view = new VelocityView("/templates%s.vm".format(helper.uri))
+    helper.response.setContentType(MediaType.TEXT_HTML)
+    view.render(Map[String,Any](), helper.response)
+  }
+}
+
