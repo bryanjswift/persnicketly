a5572eafdf0fd022cd3bcc69dde6d238311ff3b0
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-10 01:18:02 -0400

    Make bookmarks method return Bookmarks add currentUser method

--- src/main/scala/com/persnicketly/readability/Api.scala
+++ src/main/scala/com/persnicketly/readability/Api.scala
@@ -6,17 +6,27 @@ import dispatch.nio.Http
 import dispatch.oauth.Consumer
 import dispatch.oauth.OAuth.Request2RequestSigner
 import dispatch.url
-import com.persnicketly.readability.model.User
+import com.persnicketly.readability.model.{Bookmark, User, UserData}
+import com.persnicketly.readability.api.{BookmarkExtractor, UserDataExtractor}
 
 object Api {
   private val bookmarksUrl = url("https://www.readability.com/api/rest/v1/bookmarks")
-  def bookmarks(consumer: Consumer, user: User) = {
+  private val userUrl = url("https://www.readability.com/api/rest/v1/users/_current")
+  def bookmarks(consumer: Consumer, user: User): List[Bookmark] = {
     val http = new Http
-    val marks = bookmarksUrl <@ (consumer, user.accessToken.get)
-    val response = http(marks ># obj)()
+    val request = bookmarksUrl <@ (consumer, user.accessToken.get)
+    val response = http(request ># obj)()
     val bookmarkObjects = ('bookmarks ! (list ! obj))(response)
     val metaObject = ('meta ! obj)(response)
     val conditionsObject = ('conditions ! obj)(response)
+    http.shutdown
+    bookmarkObjects map BookmarkExtractor
+  }
+  def currentUser(consumer: Consumer, user: User): Option[UserData] = {
+    val http = new Http
+    val request = userUrl <@ (consumer, user.accessToken.get)
+    val response = http(request ># obj)()
+    UserDataExtractor.unapply(response)
   }
 }
 
