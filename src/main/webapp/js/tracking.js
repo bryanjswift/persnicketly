6a838c35ffb2e127c4137b7082f38e70de1a006a
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-11 00:48:31 -0400

    Reorder variable declarations and add location.search to tracking

--- src/main/webapp/js/tracking.js
+++ src/main/webapp/js/tracking.js
@@ -1,17 +1,18 @@
 (function(doc, tag, win) {
-  win.mpq = [
-    ["init","b13487272824c77655227e84f830d7ee"],
-    ["register", params],
-    ["track", "pageview", { "path": location.path || location.pathname, "domain": location.host || location.hostname }]
-  ];
   var s = doc.createElement(tag)
     , el = doc.getElementsByTagName(tag)[0]
     , nav = win.navigator
+    , l = location
     , params = {
         "userAgent": nav.userAgent
       , "language": nav.language
       , "referrer": (document.referrer || document.referer)
       };
+  win.mpq = [
+    ["init","b13487272824c77655227e84f830d7ee"],
+    ["register", params],
+    ["track", "pageview", { "path": l.path || l.pathname, "domain": l.host || l.hostname, "q": l.search }]
+  ];
   s.src = "//api.mixpanel.com/site_media/js/api/mixpanel.js";
   s.async = 1;
   if (location.href.match(/.*persnicketly\.com.*/)) {
