21e06e621bcfbd69ec49c0757f0d4cbc554de49a
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-16 01:29:39 -0400

    Add jersey serving back in

--- src/main/scala/com/persnicketly/web/PersnicketlyServletConfig.scala
+++ src/main/scala/com/persnicketly/web/PersnicketlyServletConfig.scala
@@ -2,7 +2,9 @@ package com.persnicketly.web
 
 import org.slf4j.LoggerFactory
 import com.google.inject.servlet.{GuiceServletContextListener, ServletModule}
-import com.google.inject.{Guice, Injector}
+import com.google.inject.{Guice, Injector, Singleton}
+import com.sun.jersey.spi.container.servlet.ServletContainer
+import com.sun.jersey.api.core.PackagesResourceConfig
 import scala.collection.JavaConversions.asJavaMap
 import com.persnicketly.web.servlet.{RootServlet, TemplateServlet}
 import com.persnicketly.web.servlet.readability.{CallbackServlet, LoginServlet}
@@ -17,9 +19,14 @@ private class PersnicketlyServletModule extends ServletModule {
   private val log = LoggerFactory.getLogger(classOf[PersnicketlyServletModule])
   override protected def configureServlets(): Unit = {
     StdLogRedirect.redirectLogs
+    val jerseyParams = Map(PackagesResourceConfig.PROPERTY_PACKAGES -> "com.persnicketly.web.resource,com.codahale.jersey.providers,com.codahale.jersey.providers,com.codahale.jersey.inject")
     serve("/", "/index.html").`with`(classOf[RootServlet])
     serve("/readability/login").`with`(classOf[LoginServlet])
     serve("/readability/callback").`with`(classOf[CallbackServlet])
     serve("/learn-more").`with`(classOf[TemplateServlet])
+    serve("/d/*").`with`(classOf[JerseyServletContainer], jerseyParams)
   }
 }
+
+@Singleton
+private class JerseyServletContainer extends ServletContainer { }
