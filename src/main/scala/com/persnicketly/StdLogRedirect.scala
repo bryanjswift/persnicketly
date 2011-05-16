d5036cde0a756ca03731fbe0964d57e9c13ebba7
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-16 04:11:57 -0400

    Lots of work to get stdout to redirect nicely to log4j
    stderr wasn't redirect as nicely so it is left out, but the pattern to
    attempt is similar

--- src/main/scala/com/persnicketly/StdLogRedirect.scala
+++ src/main/scala/com/persnicketly/StdLogRedirect.scala
@@ -1,22 +1,50 @@
 package com.persnicketly
 
-import java.io.PrintStream
-import org.slf4j.LoggerFactory
+import java.io.{PrintStream, OutputStream, ByteArrayOutputStream}
+import org.slf4j.{Logger, LoggerFactory}
+import com.persnicketly.web.Persnicketly
 
 object StdLogRedirect {
   def redirectLogs = {
-    /*
-    System.setOut(proxy(System.out, "stdout"))
-    System.setErr(proxy(System.err, "stderr"))
-    */
+    val stdout = System.out
+    System.setOut(proxy(stdout, "stdout", (s: String) => { stdout.println(s) }))
+    stdout.println("Redirecting stdout to slf4j")
   }
-  private def proxy(real: PrintStream, name: String): PrintStream = {
-    new PrintStream(real) {
-      private val log = LoggerFactory.getLogger(name)
-      override def print(s: String): Unit = {
-        log.info(s)
-      }
+
+  private def proxy(real: PrintStream, name: String, r: (String) => Unit): PrintStream = {
+    new LoggingPrintStream(real, new LoggingOutputStream(LoggerFactory.getLogger(name)), r)
+  }
+}
+
+class LoggingPrintStream(real: PrintStream, os: OutputStream, r: (String) => Unit) extends PrintStream(os, true) {
+
+  override def print(s: String) = {
+    val re = Persnicketly.Config("log.re").or("""INF: \[console logger\] """).r
+    if (re.findFirstIn(s).isDefined) {
+      super.print(re.replaceFirstIn(s, ""))
+    } else {
+      r(s)
     }
   }
 }
+
+class LoggingOutputStream(private val logger: Logger) extends ByteArrayOutputStream { 
+ 
+    private val lineSeparator = System.getProperty("line.separator")
+ 
+    override def flush(): Unit = {
+      this.synchronized {
+        super.flush(); 
+        val record = this.toString(); 
+        super.reset(); 
+
+        if (record.length() == 0 || record.equals(lineSeparator)) { 
+          // avoid empty records 
+          return; 
+        } 
+
+        logger.info(record); 
+      }
+    } 
+} 
 
