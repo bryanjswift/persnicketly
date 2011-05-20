1954bdca4e00e3dbc5aea50da929c437f5f1b3de
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-19 21:23:59 -0400

    Extend the status codes parsed by the JsHandler

--- src/main/scala/com/persnicketly/readability/Api.scala
+++ src/main/scala/com/persnicketly/readability/Api.scala
@@ -12,10 +12,11 @@ import com.persnicketly.readability.api.{BookmarkExtractor, MetaExtractor, UserD
 object Api {
   private val bookmarksUrl = url("https://www.readability.com/api/rest/v1/bookmarks")
   private val userUrl = url("https://www.readability.com/api/rest/v1/users/_current")
+  private val statusCodes = { code: Int => List(200, 201, 202, 203, 204, 400, 401, 403, 404, 409, 500) contains code }
   def bookmarks(consumer: Consumer, user: User): List[Bookmark] = {
     val http = new Log4jHttp
     val request = bookmarksUrl <@ (consumer, user.accessToken.get)
-    val response = http(request ># obj)()
+    val response = http.when(statusCodes)(request ># obj)()
     val bookmarkObjects = ('bookmarks ! (list ! obj))(response)
     val metaObject = ('meta ! obj)(response)
     val conditionsObject = ('conditions ! obj)(response)
@@ -25,7 +26,7 @@ object Api {
   def bookmarksMeta(consumer: Consumer, user: User): Meta = {
     val http = new Log4jHttp
     val request = bookmarksUrl <<? Map("per_page" -> "1") <@ (consumer, user.accessToken.get)
-    val response = http(request ># obj)()
+    val response = http.when(statusCodes)(request ># obj)()
     val metaObject = ('meta ! obj)(response)
     http.shutdown
     MetaExtractor(metaObject)
@@ -33,7 +34,7 @@ object Api {
   def currentUser(consumer: Consumer, user: User): Option[UserData] = {
     val http = new Log4jHttp
     val request = userUrl <@ (consumer, user.accessToken.get)
-    val response = http(request ># obj)()
+    val response = http.when(statusCodes)(request ># obj)()
     UserDataExtractor.unapply(response)
   }
 }
