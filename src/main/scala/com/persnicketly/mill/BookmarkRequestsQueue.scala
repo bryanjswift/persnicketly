7325dbe57ec9f89315376fa779653967ce9e796d
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-24 13:47:57 -0400

    Add some logic for bookmark request queing and processing

--- src/main/scala/com/persnicketly/mill/BookmarkRequestsQueue.scala
+++ src/main/scala/com/persnicketly/mill/BookmarkRequestsQueue.scala
@@ -1,16 +1,18 @@
 package com.persnicketly.mill
 
 import com.persnicketly.Persnicketly
+import com.persnicketly.readability.Api
 import com.persnicketly.readability.api.BookmarkRequestConditions
 import com.persnicketly.readability.model.{Meta,User}
+import com.persnicketly.persistence.BookmarkDao
 import org.joda.time.DateTime
 
 object BookmarkRequestsQueue extends Queue {
   val queueName = "bookmarks-requests";
-  def addAll(meta: Meta, user: User): Option[Seq[BookmarkRequestConditions]] = {
-    val perPage = Persnicketly.Config("queue." + queueName + ".perpage").or(25)
-    val numPages = scala.math.ceil(meta.totalCount / perPage).toInt
-    val conditions = for (i <- 1 to numPages) yield BookmarkRequestConditions(i, perPage, None, user)
+  def addAll(meta: Meta, user: User, since: Option[DateTime] = None): Option[Seq[BookmarkRequestConditions]] = {
+    val pageSize = Persnicketly.Config("queue." + queueName + ".perpage").or(25)
+    val numPages = scala.math.ceil(meta.totalCount / pageSize).toInt
+    val conditions = for (i <- 1 to numPages) yield BookmarkRequestConditions(i, pageSize, since, user)
     withChannel(config) { channel =>
       conditions.foreach(condition => {
         log.info("Adding {) to {} queue", condition, config.name)
@@ -19,7 +21,12 @@ object BookmarkRequestsQueue extends Queue {
       conditions
     }
   }
-  def add(since: DateTime, user: User): Option[BookmarkRequestConditions] = {
-    None
+  def add(since: DateTime, user: User): Option[Seq[BookmarkRequestConditions]] = {
+    val meta = Api.bookmarksMeta(Persnicketly.oauthConsumer, user, Some(since))
+    addAll(meta, user, Some(since))
+  }
+  def process(conditions: BookmarkRequestConditions): Unit = {
+    val bookmarks = Api.bookmarks(Persnicketly.oauthConsumer, conditions)
+    bookmarks.foreach(BookmarkDao.save(_))
   }
 }
