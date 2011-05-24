4b6d6d203055102fe26ab350db04d001c72632c6
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-24 12:12:07 -0400

    DRY up the request process by passing around anonymous functions

--- src/main/scala/com/persnicketly/readability/Api.scala
+++ src/main/scala/com/persnicketly/readability/Api.scala
@@ -1,11 +1,11 @@
 package com.persnicketly.readability
 
+import dispatch.{nio, Request, url}
+import dispatch.json.JsObject
 import dispatch.json.Js._
 import dispatch.json.JsHttp.requestToJsHandlers
-import dispatch.nio
 import dispatch.oauth.Consumer
 import dispatch.oauth.OAuth.Request2RequestSigner
-import dispatch.url
 import com.persnicketly.readability.model.{Bookmark, Meta, User, UserData}
 import com.persnicketly.readability.api.{BookmarkExtractor, MetaExtractor, UserDataExtractor}
 
@@ -15,28 +15,30 @@ object Api {
   private val statusCodes = { code: Int => List(200, 201, 202, 203, 204, 400, 401, 403, 404, 409, 500) contains code }
   val datePattern = "YYYY-MM-dd HH:mm:ss"
   def bookmarks(consumer: Consumer, user: User): List[Bookmark] = {
-    val http = new Log4jHttp
-    val request = bookmarksUrl <@ (consumer, user.accessToken.get)
-    val response = http.when(statusCodes)(request ># obj)()
-    val bookmarkObjects = ('bookmarks ! (list ! obj))(response)
-    val metaObject = ('meta ! obj)(response)
-    val conditionsObject = ('conditions ! obj)(response)
-    http.shutdown
-    bookmarkObjects map BookmarkExtractor
+    request(bookmarksUrl, consumer, user) { response =>
+      val bookmarkObjects = ('bookmarks ! (list ! obj))(response)
+      bookmarkObjects map BookmarkExtractor
+    }
   }
   def bookmarksMeta(consumer: Consumer, user: User): Meta = {
-    val http = new Log4jHttp
-    val request = bookmarksUrl <<? Map("per_page" -> "1") <@ (consumer, user.accessToken.get)
-    val response = http.when(statusCodes)(request ># obj)()
-    val metaObject = ('meta ! obj)(response)
-    http.shutdown
-    MetaExtractor(metaObject)
+    val url = bookmarksUrl <<? Map("per_page" -> "1")
+    request(url, consumer, user) { response =>
+      val metaObject = ('meta ! obj)(response)
+      MetaExtractor(metaObject)
+    }
   }
   def currentUser(consumer: Consumer, user: User): Option[UserData] = {
+    request(userUrl, consumer, user) { response =>
+      UserDataExtractor.unapply(response)
+    }
+  }
+  private def request[T](url: Request, consumer: Consumer, user: User)(thunk: JsObject => T) = {
     val http = new Log4jHttp
     val request = userUrl <@ (consumer, user.accessToken.get)
     val response = http.when(statusCodes)(request ># obj)()
-    UserDataExtractor.unapply(response)
+    val result = thunk(response)
+    http.shutdown
+    result
   }
 }
 
