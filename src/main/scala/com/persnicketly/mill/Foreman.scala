bbcce1dd0e1ca6e003e19fcbc9563b672b5ee3d6
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-26 01:54:47 -0400

    Add all existing users back into the queue on startup

--- src/main/scala/com/persnicketly/mill/Foreman.scala
+++ src/main/scala/com/persnicketly/mill/Foreman.scala
@@ -1,6 +1,7 @@
 package com.persnicketly.mill
 
 import com.persnicketly.{Command,Persnicketly}
+import com.persnicketly.persistence.UserDao
 
 object Foreman extends Command {
   val group = new ThreadGroup("Mill")
@@ -10,6 +11,8 @@ object Foreman extends Command {
     // start up consumers
     val userConsumer = async("User Consumer", UserQueue.startConsumer)
     val bookmarkRequestConsumer = async("Bookmark Reqeusts Consumer", BookmarkRequestsQueue.startConsumer)
+    // re-add all existing users
+    UserDao.all.foreach(UserQueue.add)
     // join consumers to main thread
     List(userConsumer, bookmarkRequestConsumer)
   }
