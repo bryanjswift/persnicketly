046ddf23a43c9bafd5b4f256f280ac765aaa6f1b
Bryan J Swift <bryan.j.swift@gmail.com>
2011-05-26 03:36:02 -0400

    Only process users on startup that haven't been processed recently

--- src/main/scala/com/persnicketly/mill/Foreman.scala
+++ src/main/scala/com/persnicketly/mill/Foreman.scala
@@ -2,6 +2,8 @@ package com.persnicketly.mill
 
 import com.persnicketly.{Command,Persnicketly}
 import com.persnicketly.persistence.UserDao
+import com.persnicketly.readability.model.User
+import org.joda.time.DateTime
 
 object Foreman extends Command {
   val group = new ThreadGroup("Mill")
@@ -12,8 +14,13 @@ object Foreman extends Command {
     val userConsumer = async("User Consumer", UserQueue.startConsumer)
     val bookmarkRequestConsumer = async("Bookmark Reqeusts Consumer", BookmarkRequestsQueue.startConsumer)
     // re-add all existing users
-    UserDao.all.foreach(UserQueue.add)
+    usersToUpdate.foreach(UserQueue.add)
     // join consumers to main thread
     List(userConsumer, bookmarkRequestConsumer)
+  }
+
+  def usersToUpdate: List[User] = {
+    val threshold = (new DateTime).minusHours(16)
+    UserDao.all.filter(u => !u.lastProcessed.isDefined || u.lastProcessed.get.isBefore(threshold))
   }
 }
