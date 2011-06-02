eaf207c9f43d0a36ae7d745d7d9c9933739c434b
Bryan J Swift <bryan.j.swift@gmail.com>
2011-06-01 22:05:25 -0400

    Add DAO with accecss to bookmarks collection to aggregate it into articles

---
+++ src/main/scala/com/persnicketly/persistence/ScoredArticleDao.scala
@@ -0,0 +1,62 @@
+package com.persnicketly.persistence
+
+import com.mongodb.casbah.Imports._
+import com.mongodb.casbah.commons.conversions.scala._
+import com.persnicketly.{Logging, Persnicketly}
+import com.persnicketly.model.ScoredArticle
+import com.persnicketly.readability.model.Article
+import org.bson.types.ObjectId
+
+class ScoredArticleDao extends Logging {
+  import ScoredArticleDao._
+  import Persnicketly.Config
+  private val addresses = Config("db.hosts").or(List(ServerAddress("localhost", 27017)))
+  val connection = Connection(addresses.map(_.mongo))
+  val articles = connection(Config("db.name").or("persnicketly_test"))("articles")
+  val bookmarks = connection(Config("db.name").or("persnicketly_test"))("bookmarks")
+
+  // DBObject types for getting aggregate data
+  private val key = MongoDBObject("article_id" -> 1, "favorite" -> 1, "article_title" -> 1, "article_url" -> 1, "article_excerpt" -> 1)
+  private val cond = MongoDBObject()
+  private val initial = MongoDBObject("score" -> 0)
+  private val reduce = "function(o,p) { if (o.favorite) { p.score += 2; } else { p.score += 1; } }"
+
+  def all(): List[ScoredArticle] = {
+    val articles = bookmarks.group(key, cond, initial, reduce)
+    articles.map(dbobject2article).toList
+  }
+
+  def save(scored: ScoredArticle): ScoredArticle = {
+    val query = scored.id match {
+      case Some(id) => MongoDBObject("_id" -> id)
+      case None => MongoDBObject("article_id" -> scored.article.articleId)
+    }
+    articles.update(query, scored, upsert = true, multi = false)
+    articles.findOne(query).get
+  }
+}
+
+object ScoredArticleDao {
+  implicit def article2dbobject(scored: ScoredArticle): DBObject = {
+    val builder = MongoDBObject.newBuilder
+    scored.id.foreach(id => builder += ("_id" -> id))
+    builder += "article_id" -> scored.article.articleId
+    builder += "article_title" -> scored.article.title
+    builder += "article_url" -> scored.article.url
+    scored.article.excerpt.foreach(ex => builder += ("article_excerpt" -> ex))
+    builder += "score" -> scored.score
+    builder.result
+  }
+  implicit def dbobject2article(o: DBObject): ScoredArticle = {
+    ScoredArticle(
+      o._id,
+      Article(
+        o.getAsOrElse("article_id", ""),
+        o.getAsOrElse("article_title", ""),
+        o.getAsOrElse("article_url", ""),
+        o.getAs[String]("article_excerpt")
+      ),
+      o.getAsOrElse("score", 0.0)
+    )
+  }
+}
