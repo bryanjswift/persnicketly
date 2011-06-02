3abcd684f8c171d14148d8db675d864bafc6ec6d
Bryan J Swift <bryan.j.swift@gmail.com>
2011-06-02 10:35:41 -0400

    First cut at Article listing servlet

---
+++ src/main/scala/com/persnicketly/web/servlet/ArticlesServlet.scala
@@ -0,0 +1,20 @@
+package com.persnicketly.web.servlet
+
+import com.google.inject.Singleton
+import com.persnicketly.{Logging, Persnicketly}
+import com.persnicketly.web.Servlet
+import com.persnicketly.persistence.ScoredArticleDao
+import velocity.VelocityView
+import javax.ws.rs.core.MediaType
+
+@Singleton
+class ArticlesServlet extends Servlet with Logging {
+
+  override def doGet(helper: HttpHelper) {
+    val userId = helper.cookie("_user")
+    val articles = ScoredArticleDao.all()
+    val view = new VelocityView("/templates/articleList.vm")
+    helper.response.setContentType(MediaType.TEXT_HTML)
+    view.render(Map[String,Any]("articles" -> articles, "user" -> userId), helper.response)
+  }
+}
