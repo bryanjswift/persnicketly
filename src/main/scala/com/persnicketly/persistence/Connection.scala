ac0e2f1d57c7d4cf48104091ad36fcb6ae627935
Bryan J Swift <bryan.j.swift@gmail.com>
2011-06-02 21:15:12 -0400

    Make it so there is only one connection to the MongoDB in memory
    Import the single mongo connection into each Dao to get a reference to
    the collections.

--- src/main/scala/com/persnicketly/persistence/Connection.scala
+++ src/main/scala/com/persnicketly/persistence/Connection.scala
@@ -2,8 +2,12 @@ package com.persnicketly.persistence
 
 import com.mongodb.{ServerAddress => MongoDBAddress}
 import com.mongodb.casbah.Imports._
+import com.persnicketly.Persnicketly.Config
 
 object Connection {
+  private val addresses = Config("db.hosts").or(List(ServerAddress("localhost", 27017)))
+  lazy val mongo = Connection(addresses.map(_.mongo))
+
   def apply(hosts: List[MongoDBAddress]) = {
     if (hosts.length == 1) { MongoConnection(hosts.head) }
     else { MongoConnection(hosts) }
--- src/main/scala/com/persnicketly/persistence/Dao.scala
+++ src/main/scala/com/persnicketly/persistence/Dao.scala
@@ -4,13 +4,13 @@ import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.commons.conversions.scala._
 import com.persnicketly.{Logging, Persnicketly}
 
+import Persnicketly.Config
+
 trait Dao extends Logging {
-  import Persnicketly.Config
+  import Connection.mongo
   RegisterJodaTimeConversionHelpers()
 
   def collectionName: String
 
-  private val addresses = Config("db.hosts").or(List(ServerAddress("localhost", 27017)))
-  lazy val connection = Connection(addresses.map(_.mongo))
-  lazy val collection = connection(Config("db.name").or("persnicketly_test"))(collectionName)
+  lazy val collection = mongo(Config("db.name").or("persnicketly_test"))(collectionName)
 }
