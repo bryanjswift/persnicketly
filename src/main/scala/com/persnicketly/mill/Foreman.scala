136f9ccaf0bcca7815e377d5d5a58ddb2d9e3410
Bryan J Swift <bryan.j.swift@gmail.com>
2011-06-04 18:52:26 -0400

    Enable Foreman to schedule periodic updates to data

--- src/main/scala/com/persnicketly/mill/Foreman.scala
+++ src/main/scala/com/persnicketly/mill/Foreman.scala
@@ -1,20 +1,36 @@
 package com.persnicketly.mill
 
 import com.persnicketly.{Command,Persnicketly}
-import com.persnicketly.persistence.UserDao
+import com.persnicketly.persistence.{ScoredArticleDao, UserDao}
 import com.persnicketly.readability.model.User
 import org.joda.time.DateTime
+import java.util.concurrent.{ScheduledThreadPoolExecutor, TimeUnit}
 
 object Foreman extends Command {
   val group = new ThreadGroup("Mill")
+  val executor = new ScheduledThreadPoolExecutor(4)
 
   def start(options: CliOpts): List[Thread] = {
     val config = Persnicketly.Config
     // start up consumers
     val userConsumer = async("User Consumer", UserQueue.startConsumer)
     val bookmarkRequestConsumer = async("Bookmark Reqeusts Consumer", BookmarkRequestsQueue.startConsumer)
-    // re-add all existing users
-    usersToUpdate.foreach(UserQueue.add)
+    // Schedule tasks
+    if (options.hasOption("scheduled")) {
+      log.info("Scheduling Mill updates")
+      executor.scheduleWithFixedDelay(new Runnable {
+          override def run() {
+            log.info("Processing users")
+            Foreman.usersToUpdate.foreach(UserQueue.add)
+          }
+        }, 1L, 8L, TimeUnit.HOURS)
+      executor.scheduleWithFixedDelay(new Runnable {
+          override def run() {
+            log.info("Updating article scores")
+            ScoredArticleDao.update()
+          }
+        }, 1L, 4L, TimeUnit.HOURS)
+    }
     // join consumers to main thread
     List(userConsumer, bookmarkRequestConsumer)
   }
