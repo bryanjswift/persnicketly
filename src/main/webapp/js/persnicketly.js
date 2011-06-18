285c4890b1092c603d6e64d1d77b40e38018189a
Bryan J Swift <bryan.j.swift@gmail.com>
2011-06-18 14:46:42 -0400

    Make the content area resize so it fills the browser window
    If it doesn't already. This keeps the footer pinned to the bottom

--- src/main/webapp/js/persnicketly.js
+++ src/main/webapp/js/persnicketly.js
@@ -1,9 +1,26 @@
 (function() {
-  $.script.path = '/js/';
-  $.domReady(evenArticles);
   var win = $(window)
     , t = false;
-  function evenArticles() {
+
+  $.script.path = '/js/';
+  $.domReady(resizeArticles);
+  win.addListener('resize', function onResize() {
+    if (t) { clearTimeout(t); }
+    t = setTimeout(function resizeTimeout() {
+      win.trigger('resized');
+      t = false;
+    }, 100);
+  });
+  win.addListener('resized', function onResized() {
+    resetAndResizeArticles();
+    resetAndResizeContent();
+  });
+  win.addListener('load', resizeContent);
+
+  function resetArticles() {
+    $('.list li').attr('style', '');
+  }
+  function resizeArticles() {
     $('.list li').forEach(function(a) {
       var el = $(a)
         , neighbor = el.next()
@@ -17,17 +34,22 @@
       neighbor.height(h + 'px');
     });
   }
-  function resetArticles() {
-    $('.list li').attr('style', '');
+  function resetAndResizeArticles() {
+    resetArticles();
+    if (win.width() > 850) {
+      resizeArticles();
+    }
   }
-  $(window).addListener('resize', function resizeTest() {
-    if (t) { clearTimeout(t); }
-    t = setTimeout(function resizeTimeout() {
-      resetArticles();
-      if (win.width() > 850) {
-        evenArticles();
-      }
-      t = false;
-    }, 100);
-  });
+  function resizeContent() {
+    var diff = win.height() - $('#wrapper').height()
+      , content = $('#content')
+      , contentH = content.height();
+    if (diff > 0) {
+      content.height((contentH + diff) + 'px');
+    }
+  }
+  function resetAndResizeContent() {
+    $('#content').attr('style', '');
+    resizeContent();
+  }
 })();
