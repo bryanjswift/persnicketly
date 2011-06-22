15a1b96cc9e5b094afb7fee4df1acd9d79074e3c
Bryan J Swift <bryan.j.swift@gmail.com>
2011-06-22 16:43:03 -0400

    Change how Api methods are organized
    Add Bookmarks.favorite method that marks a given bookmark as favorite

--- src/main/scala/com/persnicketly/readability/Api.scala
+++ src/main/scala/com/persnicketly/readability/Api.scala
@@ -17,21 +17,46 @@ object Api extends Logging {
   private val statusCodes = { code: Int => List(200, 201, 202, 203, 204, 400, 401, 403, 404, 409, 500) contains code }
   private val errorExtractor = 'error ?? bool
   val datePattern = "YYYY-MM-dd HH:mm:ss"
-  def bookmarks(consumer: Consumer, conditions: BookmarkRequestConditions): Option[List[Bookmark]] = {
-    var url = bookmarksUrl <<? conditions.map
-    request(url, consumer, conditions.user) { response =>
-      val bookmarkObjects = ('bookmarks ! (list ! obj))(response)
-      bookmarkObjects map BookmarkExtractor
+
+  object Bookmarks {
+    def add(consumer: Consumer, user: User, article: Article): Unit = bookmark(consumer, user, article.url)
+    def add(consumer: Consumer, user: User, pageUrl: String): Unit = {
+      val url = bookmarksUrl << Map("url" -> pageUrl)
+      // article adding gives an empty response which dispatch translates to null
+      request(url, consumer, user) { response => response }
     }
+    def favorite(consumer: Consumer, mark: Bookmark, user: User): Option[Bookmark] = {
+      val url = bookmarksUrl / mark.bookmarkId.toString <<? Map("favorite" -> "1")
+      request(url, consumer, user) { response =>
+        BookmarkExtractor(response)
+      }
+    }
+    def fetch(consumer: Consumer, conditions: BookmarkRequestConditions): Option[List[Bookmark]] = {
+      var url = bookmarksUrl <<? conditions.map
+      request(url, consumer, conditions.user) { response =>
+        val bookmarkObjects = ('bookmarks ! (list ! obj))(response)
+        bookmarkObjects map BookmarkExtractor
+      }
+    }
+    def meta(consumer: Consumer, user: User, since: Option[DateTime] = None): Option[Meta] = {
+      var url = bookmarksUrl <<? Map("per_page" -> "1")
+      since.foreach(s => url = url <<? Map("updated_since" -> s.toString(datePattern)))
+      request(url, consumer, user) { response =>
+        val metaObject = ('meta ! obj)(response)
+        MetaExtractor(metaObject)
+      }
+    }
+  }
+
+  def bookmarks(consumer: Consumer, conditions: BookmarkRequestConditions): Option[List[Bookmark]] = {
+    Bookmarks.fetch(consumer, conditions)
   }
   def bookmarksMeta(consumer: Consumer, user: User, since: Option[DateTime] = None): Option[Meta] = {
-    var url = bookmarksUrl <<? Map("per_page" -> "1")
-    since.foreach(s => url = url <<? Map("updated_since" -> s.toString(datePattern)))
-    request(url, consumer, user) { response =>
-      val metaObject = ('meta ! obj)(response)
-      MetaExtractor(metaObject)
-    }
+    Bookmarks.meta(consumer, user, since)
   }
+  def bookmark(consumer: Consumer, user: User, article: Article): Unit = Bookmarks.add(consumer, user, article)
+  def bookmark(consumer: Consumer, user: User, pageUrl: String): Unit = Bookmarks.add(consumer, user, pageUrl)
+
   def currentUser(consumer: Consumer, user: User): Option[UserData] = {
     import scala.actors.Futures.future
     val marks = future { bookmarks(consumer, BookmarkRequestConditions(1, user)) }
@@ -39,12 +64,7 @@ object Api extends Logging {
       UserDataExtractor(response).copy(userId = marks().flatMap(_.headOption.map(_.userId)))
     }
   }
-  def bookmark(consumer: Consumer, user: User, article: Article): Unit = bookmark(consumer, user, article.url)
-  def bookmark(consumer: Consumer, user: User, pageUrl: String): Unit = {
-    val url = bookmarksUrl << Map("url" -> pageUrl)
-    // article adding gives an empty response which dispatch translates to null
-    request(url, consumer, user) { response => response }
-  }
+
   private def request[T](url: Request, consumer: Consumer, user: User)(thunk: JsObject => T): Option[T] = {
     val http = new Log4jHttp
     val request = url <@ (consumer, user.accessToken.get)
