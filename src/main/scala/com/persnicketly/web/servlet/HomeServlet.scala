8bab589f596106fdd0c8a43f9f6660e34d8ef504
Bryan J Swift <bryan.j.swift@gmail.com>
2011-12-18 23:55:09 -0800

    Code style changes

--- src/main/scala/com/persnicketly/web/servlet/HomeServlet.scala
+++ src/main/scala/com/persnicketly/web/servlet/HomeServlet.scala
@@ -13,7 +13,7 @@ import javax.ws.rs.core.MediaType
 @Singleton
 class HomeServlet extends Servlet with Logging {
 
-  override def doGet(helper: HttpHelper): Unit = {
+  override def doGet(helper: HttpHelper) {
     helper.uri match {
       case "/" =>
         helper.cookie(Constants.UserCookie) match {
@@ -27,13 +27,16 @@ class HomeServlet extends Servlet with Logging {
       case _ => HomeServlet.render(helper)
     }
   }
+
 }
 
 object HomeServlet extends Logging {
-  def render(helper: Servlet#HttpHelper): Unit = {
+
+  def render(helper: Servlet#HttpHelper) {
     log.info("Rendering view - /templates/index.vm")
     val view = new VelocityView("/templates/index.vm")
     helper.response.setContentType(MediaType.TEXT_HTML)
     view.render(Map[String,Any]("user" -> helper.cookie(Constants.UserCookie), "uri" -> helper.uri), helper.response)
   }
+
 }
