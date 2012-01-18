d8bc7cf4ec3755c13c62b454501be0a96e3b550a
Bryan J Swift <bryan.j.swift@gmail.com>
2012-01-18 15:56:55 -0500

    Add a mongodb shell script to get the articles list up to date

---
+++ config/batch-articles.js
@@ -0,0 +1,16 @@
+db.articles.drop();
+db.createCollection('articles');
+db.articles.ensureIndex({ 'article_id': 1 }, { unique: true, dropDups: true });
+var marks = db.bookmarks.find();
+marks.forEach(function(mark) {
+  var q = { 'article_id': mark.article_id };
+  var o = {
+    'article_id': mark.article_id,
+    'article_title': mark.article_title,
+    'article_domain': mark.article_domain,
+    'article_url': mark.article_url,
+    'article_excerpt': mark.article_excerpt,
+    'article_processed': mark.article_processed
+  };
+  db.articles.update(q, o, true, false);
+})
