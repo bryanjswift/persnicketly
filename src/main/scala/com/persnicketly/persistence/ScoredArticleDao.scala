c64143902d785188887d2b43d2823093a7de8512
Bryan J Swift <bryan.j.swift@gmail.com>
2012-01-18 12:13:14 -0500

    Remove all ArticleDao functionality from ScoredArticleDao
    ScoredArticleDao now focuses on fetching scored information from the
    MapReduce created collections.

--- src/main/scala/com/persnicketly/persistence/ScoredArticleDao.scala
+++ src/main/scala/com/persnicketly/persistence/ScoredArticleDao.scala
@@ -6,51 +6,22 @@ import com.persnicketly.model.ScoredArticle
 import org.scala_tools.time.Imports._
 import org.joda.time.DateTime
 
-object ScoredArticleDao extends Dao {
-  val collectionName = "articles"
+class ScoredArticleDao(from: Int) extends Dao {
+  val collectionName = "scored_" + from
 
-  val saveTimer = metrics.timer("articles-save")
-  val selectTimer = metrics.timer("articles-select")
-
-  val defaultSort = MongoDBObject("favorite_count" -> -1, "count" -> -1, "score" -> -1)
-  val scoredSort = MongoDBObject("value.favorite_count" -> -1, "value.count" -> -1, "value.score" -> -1)
-
-  // Initialize indices
-  collection.ensureIndex(MongoDBObject("article_id" -> 1))
-
-  def all(): List[ScoredArticle] = {
-    log.debug("Fetching all scored articles")
-    val articles = collection.find().sort(MongoDBObject("score" -> -1))
-    articles.map(ScoredArticle.apply).toList
-  }
-
-  def find(count: Int): List[ScoredArticle] = {
-    log.debug("Fetching the top {} articles", count)
-    val articles = collection.find("score" $gt 2).limit(count).sort(defaultSort)
-    articles.map(ScoredArticle.apply).toList
-  }
-
-  def get(articleId: String): Option[ScoredArticle] = {
-    collection.findOne(MongoDBObject("article_id" -> articleId)).map(ScoredArticle.apply)
-  }
+  val selectTimer = metrics.timer("scored_" + from + "-select")
 
   def select(from: Int, count: Int): List[ScoredArticle] = {
     selectTimer.time {
       val collection = db("scored_" + from)
-      val articles = collection.find().limit(count).sort(scoredSort)
+      val articles = collection.find().limit(count).sort(BookmarkDao.scoredSort)
       articles.map(o => ScoredArticle(o.getAs[DBObject]("value").get)).toList
     }
   }
+}
 
-  def save(scored: ScoredArticle): ScoredArticle = {
-    saveTimer.time {
-      val query = scored.id match {
-        case Some(id) => MongoDBObject("_id" -> id)
-        case None => MongoDBObject("article_id" -> scored.article.articleId)
-      }
-      log.info("Saving query -- {}", query)
-      collection.update(query, scored, upsert = true, multi = false)
-      collection.findOne(query).get
-    }
-  }
+object ScoredArticleDao {
+  private val daos = Map(14 -> new ScoredArticleDao(14), 60 -> new ScoredArticleDao(60))
+
+  def select(from: Int, count: Int): List[ScoredArticle] = daos.get(from).get.select(from, count)
 }
