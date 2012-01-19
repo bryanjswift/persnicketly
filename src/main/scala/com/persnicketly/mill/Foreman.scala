b0398383bde36f5abd17b37131b1f168bb001f7b
Bryan J Swift <bryan.j.swift@gmail.com>
2012-01-19 17:09:40 -0500

    Add support for RssArticle ranking job
    Clean up mystery exceptions wording

--- src/main/scala/com/persnicketly/mill/Foreman.scala
+++ src/main/scala/com/persnicketly/mill/Foreman.scala
@@ -1,7 +1,7 @@
 package com.persnicketly.mill
 
 import com.persnicketly.{Command,Persnicketly}
-import com.persnicketly.persistence.{BookmarkDao, ScoredArticleDao, UserDao}
+import com.persnicketly.persistence.{BookmarkDao, RssArticleDao, ScoredArticleDao, UserDao}
 import com.persnicketly.readability.model.User
 import org.joda.time.DateTime
 import java.util.concurrent.{ScheduledThreadPoolExecutor, TimeUnit}
@@ -29,8 +29,8 @@ object Foreman extends Command {
               log.info("Processing users")
               Foreman.usersToUpdate.foreach(UserQueue.add)
             } catch {
-              case e: Exception => log.error("Unable to compute scores", e)
-              case _ => log.error("Mystery exception")
+              case e: Exception => log.error("Unable to process users", e)
+              case _ => log.error("Mystery exception while processing users")
             }
           }
         }, 0L, 4L, TimeUnit.HOURS)
@@ -40,9 +40,19 @@ object Foreman extends Command {
               log.info("Computing recent scores")
               config("compute").or(Array(14)).foreach(c => BookmarkDao.compute(c))
               log.info("Finished with articles and scores")
+              // Queue an event saying there are new scores
             } catch {
               case e: Exception => log.error("Unable to compute scores", e)
-              case _ => log.error("Mystery exception")
+              case _ => log.error("Mystery exception while computing scores")
+            }
+
+            try {
+              log.info("Updating RSS Article ranks")
+              RssArticleDao.update()
+              log.info("Finished updating RSS Article ranks")
+            } catch {
+              case e: Exception => log.error("Unable to update RSS ranks", e)
+              case _ => log.error("Mystery exception while updating RSS ranks")
             }
           }
         }, 0L, 5L, TimeUnit.HOURS)
