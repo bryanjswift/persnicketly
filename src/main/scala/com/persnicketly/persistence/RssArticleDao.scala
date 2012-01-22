6f8025c2314389ea9e2959a18418693e4d454f39
Bryan J Swift <bryan.j.swift@gmail.com>
2012-01-22 16:43:38 -0500

    Abstract out sorting
    Handle the new scored attribute of RssArticle more gracefully

--- src/main/scala/com/persnicketly/persistence/RssArticleDao.scala
+++ src/main/scala/com/persnicketly/persistence/RssArticleDao.scala
@@ -7,16 +7,20 @@ import org.joda.time.DateTime
 object RssArticleDao extends Dao {
   val collectionName = "rss_articles"
 
+  val defaultSort = MongoDBObject("r" -> 1)
+
   // Initialize indices
-  collection.ensureIndex(MongoDBObject("r" -> 1))
+  collection.ensureIndex(defaultSort)
 
   val maxRank = java.lang.Integer.MAX_VALUE
 
-  def all: List[RssArticle] = collection.find().map(RssArticle.apply).toList
+  def all: List[RssArticle] = collection.find().sort(defaultSort).map(RssArticle.apply).toList
 
-  def get(articleId: String): Option[RssArticle] = {
+  def select(count: Int): List[RssArticle] =
+    collection.find("r" $lte count).sort(defaultSort).map(RssArticle.apply).toList
+
+  def get(articleId: String): Option[RssArticle] =
     collection.findOne(MongoDBObject("_id" -> articleId)).map(RssArticle.apply)
-  }
 
   def save(article: RssArticle): RssArticle = {
     val query = MongoDBObject("_id" -> article.id)
@@ -27,15 +31,18 @@ object RssArticleDao extends Dao {
   }
 
   def update(): List[RssArticle] = {
-    var existing = all.map(r => (r.id, RssArticle(r.id, None, r.rank, r.updated))).toMap
+    var existing = all.map(r => (r.id, RssArticle(r.id, None, r.rank, r.updated, r.scored))).toMap
     val articles = ScoredArticleDao.select(from = 60, count = 100).map(_.article)
     var i = 0;
     val now = new DateTime
     articles.foreach(a => {
       i = i + 1
       val id = a.articleId
-      val raw = existing.getOrElse(Some(id), RssArticle(Some(id), None, None, Some(now))).copy(rank = Some(i))
-      val article = 
+
+      val raw: RssArticle =
+        existing.getOrElse(Some(id), RssArticle(Some(id), Some(now))).copy(rank = Some(i))
+
+      val article =
         if (raw.rank.isDefined && raw.lastRank.isDefined && raw.rank.get < 10 && raw.lastRank.get > 10) {
           raw.copy(updated = Some(now))
         } else {
