0fad18a4b7d10e5458e2111e11114ddcd0cede3d
Bryan J Swift <bryan.j.swift@gmail.com>
2012-02-16 22:30:42 -0500

    Add redis client as a dependency

--- project/build/PersnicketlyWebProject.scala
+++ project/build/PersnicketlyWebProject.scala
@@ -50,6 +50,8 @@ class PersnicketlyWebProject(info: ProjectInfo) extends DefaultWebProject(info)
   val casbahCommons = "com.mongodb.casbah" %% "casbah-commons" % casbahVersion
   // RabbitMQ
   val rabbitmq = "com.rabbitmq" % "amqp-client" % "2.7.1"
+  // Redis
+  val redis = "net.debasishg" %% "redisclient" % "2.4.2"
   // Metrics
   val metricsScala = "com.yammer.metrics" %% "metrics-scala" % "2.0.0-BETA19"
   val metricsServlet = "com.yammer.metrics" % "metrics-servlet" % "2.0.0-BETA19"
