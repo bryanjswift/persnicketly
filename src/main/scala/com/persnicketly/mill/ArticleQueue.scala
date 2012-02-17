9b5af775035a3100c9d519b3658ae10bd7785bc2
Bryan J Swift <bryan.j.swift@gmail.com>
2012-02-17 12:22:12 -0500

    Change ArticleQueue to a RedisQueue

--- src/main/scala/com/persnicketly/mill/ArticleQueue.scala
+++ src/main/scala/com/persnicketly/mill/ArticleQueue.scala
@@ -4,22 +4,19 @@ import com.persnicketly.{Persnicketly, Serializer}
 import com.persnicketly.persistence.ArticleDao
 import com.persnicketly.readability.model.{Bookmark, Article}
 
-object ArticleQueue extends Queue {
-  val queueName = "articles";
+object ArticleQueue extends RedisQueue[Article] {
+  val queueName = "articles"
+  def parser = Parse(x => Article(x))
 
-  def add(bookmark: Bookmark): Option[Bookmark] = {
-    val result = add(bookmark.article)
-    if (result.isDefined) { Some(bookmark) }
-    else { None}
-  }
+  def add(bookmark: Bookmark): Option[Bookmark] =
+    add(bookmark.article).map(_ => bookmark)
 
   def add(article: Article): Option[Article] = {
     log.debug("Adding Article({}) to queue", article.articleId)
-    publish(article.toByteArray, article)
+    publish(article)
   }
 
-  def processDelivery(delivery: Delivery): Boolean = {
-    val article = Serializer[Article](delivery.getBody)
+  def process(article: Article): Boolean = {
     log.debug("Processing delivery of Article({})", article.articleId)
     ArticleDao.save(article)
     val retrieved = ArticleDao.get(article.articleId)
