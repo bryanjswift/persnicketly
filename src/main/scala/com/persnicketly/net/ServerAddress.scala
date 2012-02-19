8946d1dddd11378cff9a9d9cbf6cf4d6976c5a3c
Bryan J Swift <bryan.j.swift@gmail.com>
2012-02-18 19:31:03 -0500

    Add a redis field
    ServerAddress can be used to get a mongo or a redis connection

--- src/main/scala/com/persnicketly/net/ServerAddress.scala
+++ src/main/scala/com/persnicketly/net/ServerAddress.scala
@@ -1,7 +1,9 @@
 package com.persnicketly.net
 
 import com.mongodb.{ServerAddress => MongoDBAddress}
+import com.persnicketly.redis.Redis
 
 case class ServerAddress(host: String, port: Int) {
   val mongo = new MongoDBAddress(host, port)
+  val redis = new Redis(host, port)
 }
