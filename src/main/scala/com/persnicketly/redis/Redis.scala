176218d96bf281cbffc2ff629ea72b194102907b
Bryan J Swift <bryan.j.swift@gmail.com>
2012-02-18 20:37:40 -0500

    Move RedisWithCodec into it's own file

--- src/main/scala/com/persnicketly/redis/Redis.scala
+++ src/main/scala/com/persnicketly/redis/Redis.scala
@@ -15,26 +15,3 @@ case class Redis(host: String, port: Int) extends Logging {
 
 }
 
-class RedisWithCodec[K, V](codec: RedisCodec[K, V], redis: Redis) extends Logging {
-
-  val client = redis.client
-  val host = redis.host
-  val port = redis.port
-
-  def connection: Option[RedisConnection[K, V]] = {
-    try { Some(client.connect(codec)) }
-    catch {
-      case e: Exception => {
-        log.warn("Unable to connect to {}", this)
-        None
-      }
-    }
-  }
-
-  def exec[T](thunk: RedisConnection[K, V] => T): Option[T] = {
-    connection.map({ connection =>
-      try { thunk(connection) } finally { connection.close }
-    })
-  }
-
-}
---
+++ src/main/scala/com/persnicketly/redis/RedisWithCodec.scala
@@ -0,0 +1,29 @@
+package com.persnicketly.redis
+
+import com.lambdaworks.redis.RedisConnection
+import com.lambdaworks.redis.codec.RedisCodec
+import com.persnicketly.Logging
+
+class RedisWithCodec[K, V](codec: RedisCodec[K, V], redis: Redis) extends Logging {
+
+  val client = redis.client
+  val host = redis.host
+  val port = redis.port
+
+  def connection: Option[RedisConnection[K, V]] = {
+    try { Some(client.connect(codec)) }
+    catch {
+      case e: Exception => {
+        log.warn("Unable to connect to {}", this)
+        None
+      }
+    }
+  }
+
+  def exec[T](thunk: RedisConnection[K, V] => T): Option[T] = {
+    connection.map({ connection =>
+      try { thunk(connection) } finally { connection.close }
+    })
+  }
+
+}
